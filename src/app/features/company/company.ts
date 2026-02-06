422658df3155123513c1705157aba46ab7ce6ea0
Juliano Almada <julianoalmada48@gmail.com>
2026-02-05 23:07:15 -0300

    chore: inserção de estilos globais e Company

--- src/app/features/company/company.ts
+++ src/app/features/company/company.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-company',
   imports: [],
   templateUrl: './company.html',
-  styleUrl: './company.scss',
+  styleUrls: ['./company.scss'],
 })
 export class Company {
 
